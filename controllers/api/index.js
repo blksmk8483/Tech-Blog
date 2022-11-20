d3546ec2859c27838502ba98e169f78f25e24d6a
Brandon Knight <knightbrandon1984@gmail.com>
2022-11-20 14:08:34 -0600

    worked on the log in and log out routes and its model, added the auth file, added the public js/logout file, started the main.handlebars file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./logInOutRoutes');
-const projectRoutes = require('./projectRoutes');
+// const projectRoutes = require('./Blank');
 
 // Include my api routes here
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+// router.use('/projects', projectRoutes);
 
 module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,21 @@
+const LogInOut = require('./LogInOut');
+
+
+module.exports = LogInOut;
+
+
+
+// // Example with more than one
+// const User = require('./User');
+// const Gallery = require('./Gallery');
+// const Painting = require('./Painting');
+
+// Gallery.hasMany(Painting, {
+//   foreignKey: 'gallery_id',
+// });
+
+// Painting.belongsTo(Gallery, {
+//   foreignKey: 'gallery_id',
+// });
+
+// module.exports = { User, Gallery, Painting };
