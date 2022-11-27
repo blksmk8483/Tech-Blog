9ec252761c396bfc9ef808b236750bcd1c73bc5c
Brandon Knight <knightbrandon1984@gmail.com>
2022-11-26 20:33:06 -0600

    cleaned up what I already have.

--- models/index.js
+++ models/index.js
@@ -1,4 +1,3 @@
-const Post = require('./Post');
 const User = require('./User');
 const Blog = require('./Blog');
 
@@ -12,21 +11,4 @@ Blog.belongsTo(User, {
     onDelete: 'CASCADE'
 })
 
-module.exports = { User, Post, Blog };
-
-
-
-// // Example with more than one
-// const User = require('./User');
-// const Gallery = require('./Gallery');
-// const Painting = require('./Painting');
-
-// Gallery.hasMany(Painting, {
-//   foreignKey: 'gallery_id',
-// });
-
-// Painting.belongsTo(Gallery, {
-//   foreignKey: 'gallery_id',
-// });
-
-// module.exports = { User, Gallery, Painting };
+module.exports = { User, Blog };
